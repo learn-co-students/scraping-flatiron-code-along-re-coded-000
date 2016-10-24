4a6b7c082dbaee32e37f1613f9193a7f1aafa0bc
ALI ALRAWI <ali.developer90@gmail.com>
2016-10-24 15:04:29 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,13 @@
-
-
+class Course
+  @@all=[]
+  attr_accessor :title,:schedule ,:description
+  def initialize
+    @@all.push(self)
+  end
+  def self.all
+    return @@all
+  end
+  def self.reset_all
+    @@all=[]
+  end
+end
