673112a0d8c53b8da85c16ffdbea34eaa9c3c5a4
asmaa saadi <razana385@gmail.com>
2017-01-23 19:05:38 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,8 +29,8 @@ class Scraper
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
-      end
-    end
+        end
+     end
   end
 
 end
